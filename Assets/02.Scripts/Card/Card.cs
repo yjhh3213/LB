748d5e3d4601dbf32e93b4dfb53e86c97dad7c5d
yjhh3213 <yjhh3213@gmail.com>
2025-11-27 15:09:07 +0900

    2025/11/27
    버튼 테두리 색깔 추가

--- Assets/02.Scripts/Card/Card.cs
+++ Assets/02.Scripts/Card/Card.cs
@@ -14,6 +14,8 @@ public class Card : MonoBehaviour
 
     [Header("GameObjet")]
     public GameObject[] SetCardobj;     // 카드오브젝트
+    public GameObject[] ButtonBGCobj;   // 카드테두리오브젝트
+    public Image[] ButtonBGC;           // 버튼 뒤에 색깔 테두리
     public GameObject Player;           // 플레이어
     public Image[] CardColor;           // 카드 테두리 색깔
     public GameObject[] CardColorobj;   // 카드 UI On/Off
@@ -89,44 +91,45 @@ public class Card : MonoBehaviour
         else { CardColorobj[6].SetActive(false); }
     }
 
+  
     // 카드의 이름, 내용 소개
-    void CardNameAndDetail(int num)
+    void CardNameAndDetail(int num, int i)
     {
         switch (num)
         {
             case 0:
-                if (ShotGunCard == 2) { CardName = "샷건개조 Level 3"; CardDetail = "기존 투사체에서 +4"; }
-                else if (ShotGunCard == 1) { CardName = "샷건개조 Level 2"; CardDetail = "기존 투사체에서 +2"; }
-                else { CardName = "샷건개조 Level 1"; CardDetail = "기존 투사체에서 +1"; }
+                if (ShotGunCard == 2) { ButtonBGC[i].color = new Color(200, 0, 248); CardName = "샷건개조 Level 3"; CardDetail = "기존 투사체에서 +4"; }
+                else if (ShotGunCard == 1) { ButtonBGC[i].color = new Color(0, 144, 248);  CardName = "샷건개조 Level 2"; CardDetail = "기존 투사체에서 +2"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "샷건개조 Level 1"; CardDetail = "기존 투사체에서 +1"; }
                 break;
             case 1:
-                if (BulletCard == 1) { CardName = "총알개조 Level 2"; CardDetail = "적 두 명 관통"; }
-                else { CardName = "총알개조 Level 1"; CardDetail = "적 한 명 관통"; }
+                if (BulletCard == 1) { ButtonBGC[i].color = new Color(0, 144, 248); CardName = "총알개조 Level 2"; CardDetail = "적 두 명 관통"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "총알개조 Level 1"; CardDetail = "적 한 명 관통"; }
                 break;
             case 2:
-                if (barrelCard == 2) { CardName = "총열개조 Level 3"; CardDetail = "탄 퍼짐 60% 감소"; }
-                else if (barrelCard == 1) { CardName = "총열개조 Level 2"; CardDetail = "탄 퍼짐 40% 감소"; }
-                else { CardName = "총열개조 Level 1"; CardDetail = "탄 퍼짐 20% 감소"; }
+                if (barrelCard == 2) { ButtonBGC[i].color = new Color(200, 0, 248); CardName = "총열개조 Level 3"; CardDetail = "탄 퍼짐 60% 감소"; }
+                else if (barrelCard == 1) { ButtonBGC[i].color = new Color(0, 144, 248); CardName = "총열개조 Level 2"; CardDetail = "탄 퍼짐 40% 감소"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "총열개조 Level 1"; CardDetail = "탄 퍼짐 20% 감소"; }
                 break;
             case 3:
-                if (weaknessCard == 2) { CardName = "약점포착 Level 3"; CardDetail = "총알 데미지 50% 증가\n공격속도 20% 증가"; }
-                else if (weaknessCard == 1) { CardName = "약점포착 Level 2"; CardDetail = "총알 데미지 40% 증가"; }
-                else { CardName = "약점포착 Level 1"; CardDetail = "총알 데미지 20% 증가"; }
+                if (weaknessCard == 2) { ButtonBGC[i].color = new Color(200, 0, 248); CardName = "약점포착 Level 3"; CardDetail = "총알 데미지 50% 증가\n공격속도 20% 증가"; }
+                else if (weaknessCard == 1) { ButtonBGC[i].color = new Color(0, 144, 248); CardName = "약점포착 Level 2"; CardDetail = "총알 데미지 40% 증가"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "약점포착 Level 1"; CardDetail = "총알 데미지 20% 증가"; }
                 break;
             case 4:
-                if (nimblestepsCard == 2) { CardName = "기민한 걸음 Level 3"; CardDetail = "이동속도 +3 증가"; }
-                else if (nimblestepsCard == 1) { CardName = "기민한 걸음 Level 2"; CardDetail = "이동속도 +2 증가"; }
-                else { CardName = "기민한 걸음 Level 1"; CardDetail = "이동속도 +1 증가"; }
+                if (nimblestepsCard == 2) { ButtonBGC[i].color = new Color(200, 0, 248); CardName = "기민한 걸음 Level 3"; CardDetail = "이동속도 +3 증가"; }
+                else if (nimblestepsCard == 1) { ButtonBGC[i].color = new Color(0, 144, 248); CardName = "기민한 걸음 Level 2"; CardDetail = "이동속도 +2 증가"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "기민한 걸음 Level 1"; CardDetail = "이동속도 +1 증가"; }
                 break;
             case 5:
-                if (QuickstepCard == 2) { CardName = "퀵 스텝 Level 3"; CardDetail = "대쉬 쿨타임 3초 감소"; }
-                else if (QuickstepCard == 1) { CardName = "퀵 스텝 Level 2"; CardDetail = "대쉬 쿨타임 2초 감소"; }
-                else { CardName = "퀵 스텝 Level 1"; CardDetail = "대쉬 쿨타임 1초 감소"; }
+                if (QuickstepCard == 2) { ButtonBGC[i].color = new Color(200, 0, 248); CardName = "퀵 스텝 Level 3"; CardDetail = "대쉬 쿨타임 3초 감소"; }
+                else if (QuickstepCard == 1) { ButtonBGC[i].color = new Color(0, 144, 248); CardName = "퀵 스텝 Level 2"; CardDetail = "대쉬 쿨타임 2초 감소"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "퀵 스텝 Level 1"; CardDetail = "대쉬 쿨타임 1초 감소"; }
                 break;
             case 6:
-                if (fastdraw == 2) { CardName = "빠른 장전 Level 3"; CardDetail = "즉시 재장전"; }
-                else if (fastdraw == 1) { CardName = "빠른 장전 Level 2"; CardDetail = "재장전시 입력해야 할 수 2개 감소"; }
-                else { CardName = "빠른 장전 Level 1"; CardDetail = "재장전시 입력해야 할 수 1개 감소"; }
+                if (fastdraw == 2) { ButtonBGC[i].color = new Color(200, 0, 248); CardName = "빠른 장전 Level 3"; CardDetail = "즉시 재장전"; }
+                else if (fastdraw == 1) { ButtonBGC[i].color = new Color(0, 144, 248); CardName = "빠른 장전 Level 2"; CardDetail = "재장전시 입력해야 할 수 2개 감소"; }
+                else { ButtonBGC[i].color = new Color(152, 248, 152); CardName = "빠른 장전 Level 1"; CardDetail = "재장전시 입력해야 할 수 1개 감소"; }
                 break;
             default:
                 CardName = "";
@@ -175,7 +178,7 @@ public class Card : MonoBehaviour
 
                 Image cardDisplay = SetCardobj[i].GetComponent<Image>();
                 if (cardDisplay != null) cardDisplay.sprite = CardImage[num];
-                CardNameAndDetail(num);
+                CardNameAndDetail(num, i);
                 SetCardobj[i].transform.GetChild(0).GetComponent<Text>().text = CardName;
                 SetCardobj[i].transform.GetChild(1).GetComponent<Text>().text = CardDetail;
 
@@ -195,6 +198,7 @@ public class Card : MonoBehaviour
                     print("Sect3 : " + Sect3);
                 }
                 SetCardobj[i].SetActive(true);
+                ButtonBGCobj[i].SetActive(true);
             }
         }
     }
@@ -233,6 +237,7 @@ public class Card : MonoBehaviour
         for (int i = 0; i < 3; i++)
         {
             SetCardobj[i].SetActive(false);
+            ButtonBGCobj[i].SetActive(false);
         }
         card_cu_delay = card_delay; //딜레이 넣기
         FindObjectOfType<Aim>().aim_ch("일반"); //에임 모양 변경
