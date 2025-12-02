3c4bc1ca7bc135cdf66657846ea75550f59c55d0
yjhh3213 <yjhh3213@gmail.com>
2025-12-02 20:36:45 +0900

    2025/12/02
    수정

--- Assets/02.Scripts/Enemy/EnemyPush.cs
+++ Assets/02.Scripts/Enemy/EnemyPush.cs
@@ -6,7 +6,13 @@ public class EnemyPush : MonoBehaviour
 {
     public float PushRadius = 0.7f;         // 거리가 PushRadius일때
     public float PushForce = 3.0f;          // PushForce만큼 밀어내기
+    EnemyStat ES;
     // Start is called before the first frame update
+    private void Start()
+    {
+        ES = GetComponent<EnemyStat>();
+    }
+
     private void FixedUpdate()
     {
         Push();
@@ -18,14 +24,17 @@ public class EnemyPush : MonoBehaviour
 
         foreach(var hit in hits)
         {
-            if (hit.gameObject == gameObject) continue;
-            if (!hit.CompareTag("Enemy") && !hit.CompareTag("Skeleton") && !hit.CompareTag("Boar")) continue;
+            if(!ES.isDead)
+            {
+                if (hit.gameObject == gameObject) continue;
+                if (!hit.CompareTag("Enemy") && !hit.CompareTag("Skeleton") && !hit.CompareTag("Boar")) continue;
 
-            // 방향 계산
-            Vector2 dir = (transform.position - hit.transform.position).normalized;
+                // 방향 계산
+                Vector2 dir = (transform.position - hit.transform.position).normalized;
 
-            // 밀어내기
-            transform.position += (Vector3)(dir * PushForce * Time.deltaTime);
+                // 밀어내기
+                transform.position += (Vector3)(dir * PushForce * Time.deltaTime);
+            }
         }
     }
 }
