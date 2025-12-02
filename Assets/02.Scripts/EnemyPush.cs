f0cb73ad604b11a22bdc3329091b3298b93eb66f
yjhh3213 <yjhh3213@gmail.com>
2025-12-02 20:07:50 +0900

    2025/12/02
    EnemyPush를 추가하므써 당구공 같은 현상이 나오지 않음
    Layer에 Enemy를 추가하므써 Enemy가 선택 되어 있으면 충돌 안남

---
+++ Assets/02.Scripts/EnemyPush.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPush : MonoBehaviour
+{
+    public float PushRadius = 0.7f;         // 거리가 PushRadius일때
+    public float PushForce = 3.0f;          // PushForce만큼 밀어내기
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        Push();
+    }
+
+    void Push()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, PushRadius);
+
+        foreach(var hit in hits)
+        {
+            if (hit.gameObject == gameObject) continue;
+            if (!hit.CompareTag("Enemy") && !hit.CompareTag("Skeleton") && !hit.CompareTag("Boar")) continue;
+
+            // 방향 계산
+            Vector2 dir = (transform.position - hit.transform.position).normalized;
+
+            // 밀어내기
+            transform.position += (Vector3)(dir * PushForce * Time.deltaTime);
+        }
+    }
+}
